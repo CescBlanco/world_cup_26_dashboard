a9bc7f69c8abded2ae4a5a514d63e01e3e758aa6
Cesc Blanco <cesc.blanco98@gmail.com>
2026-06-09 09:08:48 +0200

    feat: create name mapping

---
+++ worldcup26/streamlit/utils/mapping.py
@@ -0,0 +1,8 @@
+NAME_MAPPING = {
+    "IR Iran": "Iran",
+    "Korea Republic": "South Korea",
+    "Congo DR": "DR Congo",
+    "Côte d'Ivoire": "Ivory Coast",
+    "Bosnia-Herzegovina": "Bosnia and Herzegovina",
+    "Türkiye": "Turkey",
+}
