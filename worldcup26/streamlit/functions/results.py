cdaeecfa1f1c2e3ccd8c27809ffac9596d433c65
Cesc Blanco <cesc.blanco98@gmail.com>
2026-07-10 00:53:52 +0200

    fix: change function create inicidents fot teams

--- worldcup26/streamlit/functions/results.py
+++ worldcup26/streamlit/functions/results.py
@@ -411,7 +411,7 @@ def create_inicidents_for_teams(matchdict: dict,teams_dict_id_name: dict,players
         return False
 
     # 🔹 Events not relevant for incident timeline
-    excluded_event_types = ["FormationSet","FormationChange","SubstitutionOff","Error","Save",
+    excluded_event_types = ["FormationSet","FormationChange","SubstitutionOff","Error","Save","PenaltyFaced",
                             "SubstitutionOn","Pass","Tackle","BallTouch"]
 
     df = df[~df["type"].isin(excluded_event_types)].reset_index(drop=True).copy()
