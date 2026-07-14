c9b3e0566cfddebbb5ee58a1beeb9a37edb3db1f
Cesc Blanco <cesc.blanco98@gmail.com>
2026-07-14 23:55:33 +0200

    fix: changed function create team of the week

--- worldcup26/streamlit/functions/fixtures.py
+++ worldcup26/streamlit/functions/fixtures.py
@@ -1018,7 +1018,7 @@ def create_team_of_the_week_wc26():
         'Stage 3': '3',
         'Round of 32': '1/16',
         'Round of 16': '1/8',
-        'Quarter-finals': '1/4',
+        'Quarter-finals': 'Quarter+Finals',
         'Semi-finals': '1/2',
         'Final': 'F',
     }
