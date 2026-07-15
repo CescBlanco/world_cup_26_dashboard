6cd4b9ed4f98efa8b48c11353f006695f7fe1c5d
Cesc Blanco <cesc.blanco98@gmail.com>
2026-07-15 23:43:30 +0200

    fix: change colorteam England semifinals

--- worldcup26/streamlit/functions/results.py
+++ worldcup26/streamlit/functions/results.py
@@ -1703,6 +1703,12 @@ def prepare_data_fotmob_cache( data: dict[str, Any]) -> tuple[str,str,str,str,in
     id_home_fotmob= int(pd.DataFrame(data['header']['teams'])['id'].values[0])
     id_away_fotmob= int(pd.DataFrame(data['header']['teams'])['id'].values[1])
 
+    match_id = data["general"]["matchId"]
+    if match_id == '4653856':
+        if name_home_fotmob == "England":
+            color_home = "#FFFFFF"
+        elif name_away_fotmob == "England":
+            color_away = "#FFFFFF"
     # Create lookup dictionary
     team_dict_fotmob = {id_home_fotmob: name_home_fotmob,id_away_fotmob: name_away_fotmob}
 
